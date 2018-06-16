27911c3b48270ebd8df71be76dd56a9426ed802d
Miller Kisley <milkisly@gmail.com>
2018-06-16 12:19:19 -0300

    Model Pessoa quase completa

--- app/src/main/java/com/example/agromax/com/example/agromax/model/Pessoa.java
+++ app/src/main/java/com/example/agromax/com/example/agromax/model/Pessoa.java
@@ -1,4 +1,26 @@
 package com.example.agromax.com.example.agromax.model;
 
 public class Pessoa {
+    int idServer;
+    int id;
+    int cpf;
+    String nome;
+    String email;
+    int telefone;
+    String data;
+
+    public Pessoa(){
+
+    }
+
+    public Pessoa(int _idServer, int _id, int _cpf, String _nome, String _email, int _telefone, String _data){
+        this.idServer = _idServer;
+        this.id = _id;
+        this.cpf = _cpf;
+        this.nome = _nome;
+        this.email = _email;
+        this.telefone = _telefone;
+        this.data = _data;
+
+    }
 }
